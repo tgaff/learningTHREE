220f0930c56b2469b18b31ad1d5bf577e2fbe6e3
tgaff <tgaff@alumni.nd.edu>
2016-05-30 13:11:22 -0700

    add express server

---
+++ server.js
@@ -0,0 +1,54 @@
+// SERVER-SIDE JAVASCRIPT
+
+//require express in our app
+var express = require('express');
+// generate a new express app and call it 'app'
+var app = express();
+var bodyParser = require('body-parser');
+
+// serve static files from public folder
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+// need to add this so that we can accept request payloads
+app.use(bodyParser.json());
+
+// We're placing these under /vendor to differentiate them from our own assets
+app.use('/vendor', express.static(__dirname + '/bower_components'));
+
+// set 'html' as the engine, using ejs's renderFile function
+app.set('view engine', 'html');
+
+
+/**********
+ * ROUTES *
+ **********/
+
+/*
+ * HTML Endpoints
+ */
+
+app.get('/', function homepage (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+/*
+ * JSON API Endpoints
+ */
+
+
+
+// ALL OTHER ROUTES
+// redirect all other paths to index
+app.get('*', function homepage (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+/**********
+ * SERVER *
+ **********/
+
+// listen on port 3000
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is running on http://localhost:3000/');
+});
